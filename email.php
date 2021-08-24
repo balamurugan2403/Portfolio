0aac3f32cc49e6fe936e5efb32940381138aa4b7
balamurugan2403 <88565926+balamurugan2403@users.noreply.github.com>
2021-08-23 22:23:55 -0500

    Add files via upload
    Initial commit.

---
+++ email.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$to='bsitha2@uic.edu';
+	$subject='Contact form';
+	$message="Name: ".$name."\n"."Message: "."\n".$message;
+	// $headers="From: ".$lastname;
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From:UIC Snowmass Symposium <www@uic.edu>' . "\r\n";
+
+	// if(mail($to, $subject, $message, $headers)){
+	// 	echo "<h1> success";
+	// }
+	// else{
+	// 	echo "error";
+	// }
+	mail($to, $subject, $message, $headers);
+	header("Location: index_new1.html#contact");
+}
+?>
